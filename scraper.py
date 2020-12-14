2a0a93cf7bea81f5ecdd3bcf736c419b148bc16c
Cole Heflin <cvheflin@gmail.com>
2020-12-14 15:32:24 -0800

    updated to use env vars

--- scraper.py
+++ scraper.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from datetime import date
+from dotenv import load_dotenv
+from pathlib import Path
 import logging
 import smtplib
+import os
 
 
 # TODO:
@@ -30,47 +33,51 @@ def send_email(sender_email,sender_pass, receiver_email, email_text):
 	return None
 
 
+# loading in environment vars
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+sender_email = os.environ['SENDER_EMAIL']
+sender_pass = os.environ['SENDER_PASSWORD']
+receiver_email = os.environ['RECEIVER_EMAIL']
+# receiver_email = "cvheflin@gmail.com"
+subject = 'Botlington at your service'
+body = 'Hello! I will be sending you emails regarding updates to the vineyard vines leggings.'
+email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (sender_email, receiver_email, subject, body)
+
+print(email_text)
+send_email(sender_email, sender_pass, receiver_email, email_text)
 
 
-# sender_email = "reginald.botlington@gmail.com"
-# sender_pass = ''
-# receiver_email = "sosinclair10@gmail.com"
-# subject = 'Botlington at your service'
-# body = 'Hello! I will be sending you emails regarding updates to the vineyard vines leggings.'
-# email_text = """\
-# From: %s
-# To: %s
-# Subject: %s
+# found_size = False
+# today = date.today()
+# PATH = "C:\Program Files (x86)\chromedriver.exe"
+# URL = "https://www.vineyardvines.com/womens-pants/americana-flag-print-leggings/2P001084.html?dwvar_2P001084_color=3879#q=leggings&start=8&cgid=&format=ajax&dgrqt=true&dwvar_2P001084_color=3879"
+# driver = webdriver.Chrome(PATH)
 
-# %s
-# """ % (sender_email, receiver_email, subject, body)
+# logging.info(f'Connecting to {URL}')
+# driver.get(URL)
 
-# send_email(sender_email, sender_pass, receiver_email, email_text)
+# logging.info(f'Finding all elements with class name "available"')
+# list_of_sizes = driver.find_elements_by_class_name('available')
 
+# logging.info(f'Looping through all available sizes')
+# for size in list_of_sizes:
+# 	print(size)
+# 	if size == 'S':
+# 		found_size = True
+# 		print('Found your size! Sending email to notify.')
+# 		logging.info(f'Found correct size! Sending email to notify.')
 
-found_size = False
-today = date.today()
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-URL = "https://www.vineyardvines.com/womens-pants/americana-flag-print-leggings/2P001084.html?dwvar_2P001084_color=3879#q=leggings&start=8&cgid=&format=ajax&dgrqt=true&dwvar_2P001084_color=3879"
-driver = webdriver.Chrome(PATH)
+# logging.info(f'Closing out of web browser')
+# driver.quit()
 
-logging.info(f'Connecting to {URL}')
-driver.get(URL)
-
-logging.info(f'Finding all elements with class name "available"')
-list_of_sizes = driver.find_elements_by_class_name('available')
-
-logging.info(f'Looping through all available sizes')
-for size in list_of_sizes:
-	print(size)
-	if size == 'S':
-		found_size = True
-		print('Found your size! Sending email to notify.')
-		logging.info(f'Found correct size! Sending email to notify.')
-
-logging.info(f'Closing out of web browser')
-driver.quit()
-
-if found_size == False:
-	print(f'Did not find your size on {today}. Will try again in an hour')
-	logging.info(f'Did not find your size on {today}. Will try again in an hour')
+# if found_size == False:
+# 	print(f'Did not find your size on {today}. Will try again in an hour')
+# 	logging.info(f'Did not find your size on {today}. Will try again in an hour')
