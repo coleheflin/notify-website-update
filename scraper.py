017c90be4a138e3c9469d5e08cad76f5050392e2
Cole Heflin <cvheflin@gmail.com>
2020-12-14 15:52:59 -0800

    cleaning up and reorganizing

--- scraper.py
+++ scraper.py
@@ -8,13 +8,11 @@ import os
 
 
 # TODO:
-# set up env vars for gmail accounts, password, client secret/token
-# Create email templates for when bot fails/succeeds to find the size
 # add comments
 # Automate using lambda functions
 
 
-def send_email(sender_email,sender_pass, receiver_email, email_text):
+def send_email(SENDER_EMAIL,sender_pass, receiver_email, email_text):
 	# creates SMTP session 
 	s = smtplib.SMTP('smtp.gmail.com', 587) 
 	  
@@ -22,62 +20,78 @@ def send_email(sender_email,sender_pass, receiver_email, email_text):
 	s.starttls() 
 	  
 	# Authentication 
-	s.login(sender_email, sender_pass)
+	s.login(SENDER_EMAIL, sender_pass)
 	  
 	# sending the mail 
-	s.sendmail(sender_email, receiver_email, email_text) 
+	s.sendmail(SENDER_EMAIL, receiver_email, email_text) 
 	  
 	# terminating the session 
 	s.quit()
 
 	return None
 
-
 # loading in environment vars
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-sender_email = os.environ['SENDER_EMAIL']
-sender_pass = os.environ['SENDER_PASSWORD']
-receiver_email = os.environ['RECEIVER_EMAIL']
-# receiver_email = "cvheflin@gmail.com"
-subject = 'Botlington at your service'
-body = 'Hello! I will be sending you emails regarding updates to the vineyard vines leggings.'
-email_text = """\
-From: %s
-To: %s
-Subject: %s
-
-%s
-""" % (sender_email, receiver_email, subject, body)
-
-print(email_text)
-send_email(sender_email, sender_pass, receiver_email, email_text)
 
 
-# found_size = False
-# today = date.today()
-# PATH = "C:\Program Files (x86)\chromedriver.exe"
-# URL = "https://www.vineyardvines.com/womens-pants/americana-flag-print-leggings/2P001084.html?dwvar_2P001084_color=3879#q=leggings&start=8&cgid=&format=ajax&dgrqt=true&dwvar_2P001084_color=3879"
-# driver = webdriver.Chrome(PATH)
+def main():
+	# Constants
+	URL = "https://www.vineyardvines.com/womens-pants/americana-flag-print-leggings/2P001084.html?dwvar_2P001084_color=3879#q=leggings&start=8&cgid=&format=ajax&dgrqt=true&dwvar_2P001084_color=3879"
+	SENDER_EMAIL = os.environ['SENDER_EMAIL']
+	sender_pass = os.environ['SENDER_PASSWORD']
+	SUCCESS_RECEIVER_EMAIL = os.environ['RECEIVER_EMAIL']
+	FAIL_RECEIVER_EMAIL = os.environ['SENDER_EMAIL']
+	SUBJECT = 'Update on your Vineyard Vines leggings'
+	SUCCESS_BODY = f'I have found your vineyard vines leggings in size Small! Please visit {URL}'
+	FAIL_BODY = 'I was unable to find the vineyard vines leggings :( I will try again soon.'
+	PATH = "C:\Program Files (x86)\chromedriver.exe"
+	# Variables
+	found_size = False
+	today = date.today()
+	fail_email_text = """\
+	From: %s
+	To: %s
+	SUBJECT: %s
 
-# logging.info(f'Connecting to {URL}')
-# driver.get(URL)
+	%s
+	""" % (SENDER_EMAIL, FAIL_RECEIVER_EMAIL, SUBJECT, FAIL_BODY)
+	success_email_text = """\
+	From: %s
+	To: %s
+	SUBJECT: %s
 
-# logging.info(f'Finding all elements with class name "available"')
-# list_of_sizes = driver.find_elements_by_class_name('available')
+	%s
+	""" % (SENDER_EMAIL, SUCCESS_RECEIVER_EMAIL, SUBJECT, SUCCESS_BODY)
+	# Speciyfing to use the chrome driver
+	driver = webdriver.Chrome(PATH)
 
-# logging.info(f'Looping through all available sizes')
-# for size in list_of_sizes:
-# 	print(size)
-# 	if size == 'S':
-# 		found_size = True
-# 		print('Found your size! Sending email to notify.')
-# 		logging.info(f'Found correct size! Sending email to notify.')
+	logging.info(f'Connecting to {URL}')
+	driver.get(URL)
 
-# logging.info(f'Closing out of web browser')
-# driver.quit()
+	logging.info(f'Finding all elements with class name "available"')
+	list_of_sizes = driver.find_elements_by_class_name('available')
 
-# if found_size == False:
-# 	print(f'Did not find your size on {today}. Will try again in an hour')
-# 	logging.info(f'Did not find your size on {today}. Will try again in an hour')
+	logging.info(f'Looping through all available sizes')
+	for size in list_of_sizes:
+		print(size)
+		if size == 'S':
+			found_size = True
+			print('Found your size! Sending email to notify.')
+			logging.info(f'Found correct size! Sending email to notify.')
+			send_email(SENDER_EMAIL, sender_pass, SUCCESS_RECEIVER_EMAIL, success_email_text)
+
+	logging.info(f'Closing out of web browser')
+	driver.quit()
+
+	if found_size == False:
+		print(f'Did not find your size on {today}. Will try again in an hour')
+		logging.info(f'Did not find your size on {today}. Will try again in an hour')
+		send_email(SENDER_EMAIL, sender_pass, FAIL_RECEIVER_EMAIL, fail_email_text)
+
+if __name__ == '__main__':
+	main()
+
+
+
