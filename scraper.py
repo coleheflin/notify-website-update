0cdd1f44c302ffd40b1a2da5d3cb8db88801f544
Cole Heflin <cvheflin@gmail.com>
2020-12-13 21:28:06 -0800

    cleaning up

--- scraper.py
+++ scraper.py
@@ -7,10 +7,11 @@ import smtplib
 # TODO:
 # set up env vars for gmail accounts, password, client secret/token
 # Create email templates for when bot fails/succeeds to find the size
+# add comments
 # Automate using lambda functions
 
 
-def send_email(sender_email,receiver_email, email_text):
+def send_email(sender_email,sender_pass, receiver_email, email_text):
 	# creates SMTP session 
 	s = smtplib.SMTP('smtp.gmail.com', 587) 
 	  
@@ -18,7 +19,7 @@ def send_email(sender_email,receiver_email, email_text):
 	s.starttls() 
 	  
 	# Authentication 
-	s.login(sender_email, 'moonBotphrase99*')
+	s.login(sender_email, sender_pass)
 	  
 	# sending the mail 
 	s.sendmail(sender_email, receiver_email, email_text) 
@@ -32,6 +33,7 @@ def send_email(sender_email,receiver_email, email_text):
 
 
 # sender_email = "reginald.botlington@gmail.com"
+# sender_pass = ''
 # receiver_email = "sosinclair10@gmail.com"
 # subject = 'Botlington at your service'
 # body = 'Hello! I will be sending you emails regarding updates to the vineyard vines leggings.'
@@ -43,30 +45,32 @@ def send_email(sender_email,receiver_email, email_text):
 # %s
 # """ % (sender_email, receiver_email, subject, body)
 
-# send_email(sender_email, receiver_email, email_text)
-# found_size = False
-# today = date.today()
-# PATH = "C:\Program Files (x86)\chromedriver.exe"
-# URL = "https://www.vineyardvines.com/womens-pants/americana-flag-print-leggings/2P001084.html?dwvar_2P001084_color=3879#q=leggings&start=8&cgid=&format=ajax&dgrqt=true&dwvar_2P001084_color=3879"
-# driver = webdriver.Chrome(PATH)
+# send_email(sender_email, sender_pass, receiver_email, email_text)
 
-# logging.info(f'Connecting to {URL}')
-# driver.get(URL)
 
-# logging.info(f'Finding all elements with class name "available"')
-# list_of_sizes = driver.find_elements_by_class_name('available')
+found_size = False
+today = date.today()
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+URL = "https://www.vineyardvines.com/womens-pants/americana-flag-print-leggings/2P001084.html?dwvar_2P001084_color=3879#q=leggings&start=8&cgid=&format=ajax&dgrqt=true&dwvar_2P001084_color=3879"
+driver = webdriver.Chrome(PATH)
 
-# logging.info(f'Looping through all available sizes')
-# for size in list_of_sizes:
-# 	print(size)
-# 	if size == 'S':
-# 		found_size = True
-# 		print('Found your size! Sending email to notify.')
-# 		logging.info(f'Found correct size! Sending email to notify.')
+logging.info(f'Connecting to {URL}')
+driver.get(URL)
 
-# logging.info(f'Closing out of web browser')
-# driver.quit()
+logging.info(f'Finding all elements with class name "available"')
+list_of_sizes = driver.find_elements_by_class_name('available')
 
-# if found_size == False:
-# 	print(f'Did not find your size on {today}. Will try again in an hour')
-# 	logging.info(f'Did not find your size on {today}. Will try again in an hour')
+logging.info(f'Looping through all available sizes')
+for size in list_of_sizes:
+	print(size)
+	if size == 'S':
+		found_size = True
+		print('Found your size! Sending email to notify.')
+		logging.info(f'Found correct size! Sending email to notify.')
+
+logging.info(f'Closing out of web browser')
+driver.quit()
+
+if found_size == False:
+	print(f'Did not find your size on {today}. Will try again in an hour')
+	logging.info(f'Did not find your size on {today}. Will try again in an hour')
